3c23325e3abc76b8ff5b9afa50528996a7d7c132
刘甫 <fu.liu@midea.com>
2017-03-14 14:23:47 +0800

    222

--- HelloWorld/src/com/hotmail/liufu_china/helloworld/HelloWorld.java
+++ HelloWorld/src/com/hotmail/liufu_china/helloworld/HelloWorld.java
@@ -4,5 +4,7 @@ package com.hotmail.liufu_china.helloworld;
  * Created by liufu on 2017/3/14.
  */
 public class HelloWorld {
-
+    public static  void main (String[] args){
+        System.out.println("HelloWorld!");
+    }
 }
