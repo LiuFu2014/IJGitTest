c9c371aa9b2e7a5573c80136058b8199de7d5c6d
LiuFu <LiuFu_China@hotmail.com>
2017-03-14 16:29:18 +0800

    3333

--- HelloWorld/src/com/hotmail/liufu_china/helloworld/HelloWorld.java
+++ HelloWorld/src/com/hotmail/liufu_china/helloworld/HelloWorld.java
@@ -5,6 +5,6 @@ package com.hotmail.liufu_china.helloworld;
  */
 public class HelloWorld {
     public static  void main (String[] args){
-        System.out.println("HelloWorld!来至GitHub2");
+        System.out.println("HelloWorld!来至GitHub22");
     }
 }
